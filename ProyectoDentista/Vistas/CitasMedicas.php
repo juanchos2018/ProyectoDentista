878f04f713504a25804cd3ba704674bb84ed81d5
juanchos2018 <jcarlossenati@gmail.com>
2021-07-08 17:20:50 -0500

    fitst

---
+++ ProyectoDentista/Vistas/CitasMedicas.php
@@ -0,0 +1,34 @@
+﻿---table ips--
+
+   idips
+   idtipoequipo
+   idequipo
+   
+
+
+
+
+
+
+
+-- tabla equiposID-- 
+  -idequipo --foreana
+   idip   return idps
+
+
+tipoEquipo
+
+
+
+tabla TipoEquipos
+
+id   descripcion
+1--> ordenador
+2 --> lpatop
+3 --> impresora
+
+
+
+ 
+
+
---
+++ ProyectoDentista/Vistas/CitasMedicas/SelectCitas.php
@@ -0,0 +1,71 @@
+
+<?php 
+session_start();
+
+
+
+if(isset($_GET['txtd'])) 
+	$txtdnic =strtoupper($_GET['txtd']);
+	 else $txtdnic="";
+
+if (isset($_SESSION['codigo'])) {
+
+ ?>
+
+<?php 
+
+require_once("../../Clases/Conexion.php"); 
+	$c= new Conectar();
+	$conexion=$c->conexion();
+	$sqldienteenfermedad="SELECT Diente,Enfemedad from enfermedadiente  where Dni =$txtdnic";
+		$resultadoenfermedadienete=mysqli_query($conexion,$sqldienteenfermedad);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Usuarios</title>
+
+	
+
+<select class="form-control input-sm"  id="cbmproceso" name="cbmproceso">     	 		
+						 	
+				<?php while($verenfermedaddiente=mysqli_fetch_row($resultadoenfermedadienete)): ?>
+					<option value="<?php echo $verenfermedaddiente[0] ?>"> <?php echo $verenfermedaddiente[0]."-".$verenfermedaddiente[1]; ?> </option>
+				<?php endwhile; ?>      	 	
+
+      	 	</select>  
+
+</head>
+<body>
+
+
+<div class="container">
+
+	<select class="form-control input-sm"  id="cbmproceso" name="cbmproceso">     	 		
+						 	
+				<?php while($verenfermedaddiente=mysqli_fetch_row($resultadoenfermedadienete)): ?>
+					<option value="<?php echo $verenfermedaddiente[0] ?>"> <?php echo $verenfermedaddiente[0]."-".$verenfermedaddiente[1]; ?> </option>
+				<?php endwhile; ?>      	 	
+
+      	 	</select>  
+
+</div>
+
+
+</body>
+</html>
+
+
+
+
+
+
+<?php 
+}else
+{
+	header("location:../index.php");
+}
+ ?>
+
+
